0ca552d403a88e6a35fb0152661e8888b5c8ecd1
Murhaf Sousli <murhafsousli@gmail.com>
2016-06-09 09:31:42 +0300

    sec commit

--- src/app/noaccess/noaccess.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'forbidden',
-  template: require('./noaccess.html')
-})
-export class NoAccessCmp{
-}
--- src/app/service/models/authuser.interface.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Murhaf on 5/20/2016.
- */
-export interface IAuthUser{
-  id;
-  user_display_name;
-  roles;
-}
